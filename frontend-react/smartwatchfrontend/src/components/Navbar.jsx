e7fea059b630ec6ca04257713159384810deffd7
Anja Radulovic <anjaradulovic0507@gmail.com>
2025-02-24 21:04:45 +0100

    navbar edited

--- frontend-react/smartwatchfrontend/src/components/Navbar.jsx
+++ frontend-react/smartwatchfrontend/src/components/Navbar.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../style/navbar.css";
 
 function NavBar() {
   return (
-    <div className="navBar">
-            <a>Smartwatch Application</a>
-        </div>
+    <nav
+      className="navbar navbar-expand-lg fixed-bottom"
+      style={{ backgroundColor: "#4ab44a", padding: "5px 0" }}
+    >
+      <div className="container-fluid d-flex justify-content-between px-4">
+        <Link
+          className="nav-link text-white text-center small-link"
+          to="/dashboard"
+        >
+          <i className="fas fa-heartbeat fa-lg"></i> {/* Health ikona */}
+          <p>HEALTH</p>
+        </Link>
 
-  )
+        <Link
+          className="nav-link text-white text-center small-link"
+          to="/device"
+        >
+          <i className="fas fa-mobile-alt fa-lg"></i> {/* Device ikona */}
+          <p>DEVICE</p>
+        </Link>
+
+        <Link
+          className="nav-link text-white text-center small-link"
+          to="/report"
+        >
+          <i className="fas fa-chart-line fa-lg"></i> {/* Report ikona */}
+          <p>REPORT</p>
+        </Link>
+
+        <Link
+          className="nav-link text-white text-center small-link"
+          to="/profile"
+        >
+          <i className="fas fa-user fa-lg"></i> {/* Profile ikona */}
+          <p>PROFILE</p>
+        </Link>
+      </div>
+    </nav>
+  );
 }
 
-export default NavBar
+export default NavBar;
